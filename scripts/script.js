6af14a2bfb1d50eb00e7aa5d3abc90922a590228
sayeed-cse <sayeed.web.dev@gmail.com>
2025-03-02 02:56:11 +0600

    Time added

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,43 @@
-function taskFunc(id){
+function taskFunc(id,titleId) {
     TaskAssigned()
-    // console.log(id);
     const element = document.getElementById(id);
     element.disabled = true;
-    TotalTaskDone()
+    TotalTaskDone();
+
+    // showing in activity log
+    const titleElement = document.getElementById(titleId);
+    const title = titleElement.innerText; 
+
+    const date = new Date();
+    const timeNow = date.toLocaleTimeString()
+
+    const completedTasks = document.getElementById('Completed-tasks');
+    completedTasks.innerHTML += `
+    <div class="p-2 bg-sky-50 rounded-lg text-sm">
+        <p>You have Complete The Task ${title} at ${timeNow}</p>
+    </div>
+    `
+
 }
 
-function TaskAssigned(){
+function TaskAssigned() {
     const taskAssigned = document.getElementById('task-assigned');
     const taskAssignedText = taskAssigned.innerText;
     const taskAssignedTextNum = parseInt(taskAssignedText);
-    
-    const newTaskAssigned = taskAssignedTextNum -1;
-    taskAssigned.innerText = '0'+ newTaskAssigned
+
+    const newTaskAssigned = taskAssignedTextNum - 1;
+    taskAssigned.innerText = '0' + newTaskAssigned
 }
 
-function TotalTaskDone(){
+function TotalTaskDone() {
     const totalTaskDone = document.getElementById('total-task-done');
     const totalTaskDoneNum = parseInt(totalTaskDone.innerText);
-    const newTotalTaskDone = totalTaskDoneNum +1;
+    const newTotalTaskDone = totalTaskDoneNum + 1;
     totalTaskDone.innerText = newTotalTaskDone;
+}
+
+
+// reload page to clear prev data
+function clearAll() {
+    window.location.href = './../index.html';
 }
